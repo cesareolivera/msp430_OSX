87269e96c34453c5a589ba83d385373f0a8aef10
Cesar Olivera <ceolivera@uc.cl>
2016-07-14 12:44:53 -0400

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -5,7 +5,7 @@
 # mspdebug tilib --allow-fw-update
 # ./MSP430Flasher -n MSP430F5529
 
-msp430-gcc -Wall -Wextra --std=gnu99 -mmcu=msp430f5529 -Os -o go_msp.elf hello_msp.c
+msp430-gcc -Wall -Wextra --std=gnu99 -mmcu=msp430f5529 -Os -o go_msp.elf hello_msp.c -lm
 msp430-objcopy --output-target=elf32-msp430 go_msp.elf go_msp.bin
 mspdebug tilib "prog go_msp.bin"
 
